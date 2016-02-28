d947ec883f976dfc9db878b72f990ab8b54655a1
Jamal Natour <Jamal.Natour@fumasoftware.com>
2016-02-28 17:20:19 +0000

    fixup urls

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ $(document).ready(function(){
 
     $(document).on("masthead.data.load.done",function(e,resp){
         $.each(menubar.nav,function(){
-            var list_config  = $(this);
+            var list_config  = this;
             var list_element = $("." + list_config.name);
             console.log("Config for list: " + list_config.name);
                 $.each(list_config.root.css.classes,function(el){
