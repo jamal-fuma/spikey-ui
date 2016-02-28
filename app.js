217b24afdbc7423673ee553d5cf75ba85391f9c5
Jamal Natour <Jamal.Natour@fumasoftware.com>
2016-02-28 16:41:14 +0000

    fixup urls

--- app.js
+++ app.js
@@ -30,20 +30,22 @@ $(document).ready(function(){
     PageView.setup();
     PageView.reload();
 
-    // add css to left navbar
-    var left_nav = $('.fs-inline-list-left');
-    left_nav.addClass('col-xs-12 col-sm-10 col-md-10 col-lg-10');
-    $.each(left_nav.find('li'), function(el){
-        el.addClass('col-xs-12 col-sm-2 col-md-2 col-lg-2');
-        el.find('a').addClass('Wt-ip');
-        el.s
-    });
+    $(document).on("masthead.data.load.done",function(e,resp){
+        // add css to left navbar
+        var left_nav = $(' .fs-inline-list-left');
+        left_nav.addClass('col-xs-12 col-sm-10 col-md-10 col-lg-10');
+        $.each(left_nav.find('li'), function(el){
+            el.addClass('col-xs-12 col-sm-2 col-md-2 col-lg-2');
+            el.find('a').addClass('Wt-ip');
+            el.s
+        });
 
-    // add css to right navbar
-    var right_nav = $('.fs-inline-list-right');
-    right_nav.addClass('col-xs-12 col-sm-2 col-md-2 col-lg-2');
-    $.each(right_nav.find('li'), function(el){
-        el.addClass('col-xs-12 col-sm-2 col-md-2 col-lg-2');
-        el.find('a').addClass('Wt-ip');
+        // add css to right navbar
+        var right_nav = $(' .fs-inline-list-right');
+        right_nav.addClass('col-xs-12 col-sm-2 col-md-2 col-lg-2');
+        $.each(right_nav.find('li'), function(el){
+            el.addClass('col-xs-12 col-sm-2 col-md-2 col-lg-2');
+            el.find('a').addClass('Wt-ip');
+        });
     });
 });
